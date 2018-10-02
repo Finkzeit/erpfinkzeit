c322edd1e167b69126ce3ca7d56db9beab41e5e5
lasalesi <larsericmueller@gmail.com>
2018-10-02 15:00:23 +0200

    reverse icon colour and conditional title design

--- finkzeit/public/js/fink_common.js
+++ finkzeit/public/js/fink_common.js
@@ -1,8 +1,15 @@
 // Fink Zeit script global script inserts
-console.log("Welcome to FinkZeit");
+//console.log("Welcome to FinkZeit");
 
-navbars = document.getElementsByClassName("navbar");
-
-if (navbars.length > 0) {
-	navbars[0].style.backgroundColor = red;
+// mark navbar in specific colour
+window.onload = function () {
+	var navbars = document.getElementsByClassName("navbar");
+	if (navbars.length > 0) {
+		if ((window.location.hostname.includes("erp-test")) || (window.location.hostname.includes("localhost"))) {
+			navbars[0].style.backgroundColor = "#d68080";
+		}
+		else if ((window.location.hostname.includes("erp-ch"))) {
+			navbars[0].style.backgroundColor = "#abd680";
+		}
+	}
 }
