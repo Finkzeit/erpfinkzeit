f1e56c5304261d2f8a26a3560f887d8f15b052a9
lasalesi <larsericmueller@gmail.com>
2018-10-02 13:50:46 +0200

    common script

---
+++ finkzeit/public/js/fink_common.js
@@ -0,0 +1,8 @@
+// Fink Zeit script global script inserts
+console.log("Welcome to FinkZeit");
+
+navbars = document.getElementsByClassName("navbar");
+
+if (navbars.length > 0) {
+	navbars[0].style.backgroundColor = red;
+}
