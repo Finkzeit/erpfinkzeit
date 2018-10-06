2e70df58af48a6dcf529facd33052f682e0f3638
lasalesi <larsericmueller@gmail.com>
2018-10-06 13:04:13 +0200

    removed red title bar on localhost

--- finkzeit/public/js/fink_common.js
+++ finkzeit/public/js/fink_common.js
@@ -5,7 +5,7 @@
 window.onload = function () {
 	var navbars = document.getElementsByClassName("navbar");
 	if (navbars.length > 0) {
-		if ((window.location.hostname.includes("erp-test")) || (window.location.hostname.includes("localhost"))) {
+		if (window.location.hostname.includes("erp-test")) {
 			navbars[0].style.backgroundColor = "#d68080";
 		}
 		else if ((window.location.hostname.includes("erp-ch"))) {
