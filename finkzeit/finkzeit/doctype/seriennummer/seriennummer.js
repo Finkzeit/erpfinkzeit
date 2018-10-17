65eda31876b5c0e892862cd590993b78d2b5dbba
lasalesi <larsericmueller@gmail.com>
2018-10-17 12:24:38 +0200

    magic script

--- finkzeit/finkzeit/doctype/seriennummer/seriennummer.js
+++ finkzeit/finkzeit/doctype/seriennummer/seriennummer.js
@@ -2,7 +2,60 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Seriennummer', {
-	refresh: function(frm) {
+    refresh: function(frm) {
 
-	}
+    },
+    beschriftung: function(frm) {
+        get_in_date(frm);
+    },
+    item: function(frm) {
+        find_beschriftung(frm);
+    },
+    fink_snr: function(frm) {
+        find_beschriftung(frm);
+    }
 });
+
+function find_beschriftung(frm) {
+    if ((frm.doc.item) && (frm.doc.fink_snr)) {
+        frappe.call({
+            "method":"frappe.client.get_list",
+            "args":{
+                doctype:"Beschriftung",
+                filters: [
+                    ["item","=", frm.doc.item],
+                    ["number_start", "<=", frm.doc.fink_snr],
+                    ["number_end", ">=", frm.doc.fink_snr]
+                ],
+                fields: ["name"]
+            },
+            "callback": function(response) {
+                if (response.message) {
+                    var beschriftungen = response.message;
+                    if (beschriftungen.length > 0) {
+                            frm.set_value('beschriftung', beschriftungen[0].name);
+                    } 
+                }
+            }
+        });
+    }
+}
+
+function get_in_date(frm) {
+        if ((!frm.doc.in_date) && (frm.doc.beschriftung)) {
+        frappe.call({
+            "method": "frappe.client.get",
+            "args": {
+                    "doctype": "Beschriftung",
+                    "name": frm.doc.beschriftung
+            },
+            "callback": function(response) {
+                var beschriftung = response.message;
+
+                if (beschriftung) {
+                        frm.set_value('in_date', beschriftung.date);
+                } 
+            }
+        });
+    }
+}
