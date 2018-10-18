d49bd1fb05b00fc834bc572ddda7ed59c267400d
lasalesi <larsericmueller@gmail.com>
2018-10-18 21:05:47 +0200

    removed retailer

--- finkzeit/finkzeit/doctype/retailer/retailer.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, Fink Zeitsysteme/libracore and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Retailer', {
-	refresh: function(frm) {
-
-	}
-});
