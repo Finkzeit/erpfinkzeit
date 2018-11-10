153e7ede19822991d288d6cb63a3fea3cf3e9667
lasalesi <larsericmueller@gmail.com>
2018-11-10 22:25:23 +0100

    test auto email

--- finkzeit/finkzeit/doctype/licence/test_licence.py
+++ finkzeit/finkzeit/doctype/licence/test_licence.py
@@ -6,5 +6,23 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+# import sending emails
+from frappe.email.queue import send
+
 class TestLicence(unittest.TestCase):
 	pass
+
+def send_mail(recipient):
+    # get first sales invoice
+    sinvs = frappe.get_all("Sales Invoice", filters=None, fields=['name'])
+    if sinvs:
+        send(recipients=recipient,
+            subject="Test mail",
+            message="This is a test message",
+            reference_doctype="Sales Invoice",
+            reference_name=sinvs[0]['name'],
+            attachments={"print_format_attachment": 1, "doctype": "Sales Invoice", "name": sinvs[0]['name'], "print_format": "Standard"}
+        )
+        print("Sent")
+    return
+    
