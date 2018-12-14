ba26bee2028135dc056fc281d96e0bb4e61264d5
lasalesi <larsericmueller@gmail.com>
2018-12-14 21:12:28 +0100

    saldenliste report

---
+++ finkzeit/finkzeit/report/saldenliste_sehr_einfach/saldenliste_sehr_einfach.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2016, Fink Zeitsysteme/libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Saldenliste sehr einfach"] = {
+    "filters": [
+        {
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": get_one_month_ago(new Date())
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": new Date()
+        },
+        {
+            "fieldname":"report_type",
+            "label": __("Type"),
+            "fieldtype": "Select",
+            "options": "\nBalance Sheet\nProfit and Loss"
+        }
+	]
+}
+
+// returns the date one month ago
+function get_one_month_ago(date) {
+    d = date;
+    d.setMonth(d.getMonth() - 1);
+    return d;
+}
