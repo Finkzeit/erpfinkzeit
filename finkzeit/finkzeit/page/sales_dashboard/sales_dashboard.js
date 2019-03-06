9a59885671a981298babb96959855ea7865df650
lasalesi <larsericmueller@gmail.com>
2019-03-07 00:38:16 +0100

    data collection

--- finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.js
+++ finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.js
@@ -21,71 +21,6 @@ frappe.sales_dashboard = {
         var data = "";
         $(frappe.render_template('sales_dashboard', data)).appendTo(me.body);
         
-        /*
-        // attach button handlers
-        this.page.main.find(".btn-parse-file").on('click', function() {            
-            // get selected account
-            var account = document.getElementById("bank_account").value;
-            
-            // read the file 
-            var file = document.getElementById("input_file").files[0];
-            if (file.name.toLowerCase().endsWith(".xml")) {
-                // this is an xml file
-                
-                var content = "";
-                if (file) {
-                    // create a new reader instance
-                    var reader = new FileReader();
-                    // assign load event to process the file
-                    reader.onload = function (event) {
-                        // enable waiting gif
-                        frappe.bank_wizard.start_wait();
-                        
-                        // read file content
-                        content = event.target.result;
-                        
-                        // parse the xml content
-                        frappe.bank_wizard.parse(content, account);
-                    }
-                    // assign an error handler event
-                    reader.onerror = function (event) {
-                        frappe.msgprint(__("Error reading file"), __("Error"));
-                    }
-                    
-                    reader.readAsText(file, "ANSI");
-                }
-                else
-                {
-                    frappe.msgprint(__("Please select a file."), __("Information"));
-                }
-            } else if (file.name.toLowerCase().endsWith(".zip")) {
-                // this is a zip file
-                console.log("unzipping " + file.name + "...");
-                JSZip.loadAsync(file).then(function(zip) {
-                    // async: compile a promise to extract all contained files
-                    var promises = [];
-                    zip.forEach(function (relativePath, zipEntry) {
-                        promises.push(zipEntry.async("string").then(
-                            function (data) {
-                                return data; 
-                            })
-                        );
-                    });
-                    // on completed promise, combine content and process
-                    Promise.all(promises).then(function (list) {
-                        console.log("Promise complete!");
-                        var content = list.join("");
-                        // parse the xml content
-                        frappe.bank_wizard.parse(content, account);
-                    });
-                }, function (e) {
-                    frappe.msgprint( __("Unzip error: ") + e.message, __("Error") );
-                });
-            } else {
-                frappe.msgprint( __("Unsupported file format. Please use an xml or zip camt file"), __("Error") );
-            }
-        });
-        */
     },
     run: function() {
         frappe.sales_dashboard.make_chart();
@@ -106,94 +41,44 @@ frappe.sales_dashboard = {
                 } 
             }
         }); 
-        frappe.call({
-            method: 'erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard.get_intermediate_account',
-            callback: function(r) {
-                if ((r.message) && (r.message.account != "")) {
-                    document.getElementById("intermediate_account").value = r.message.account;
-                } else {
-                    frappe.msgprint( __("Please set the <b>intermediate bank account</b> in <a href=\"/desk#Form/ERPNextSwiss Settings\">ERPNextSwiss Settings</a>.") );
-                }
-            }
-        }); 
-        frappe.call({
-            method: 'erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard.get_payable_account',
-            callback: function(r) {
-                if ((r.message) && (r.message.account != "")) {
-                    document.getElementById("payable_account").value = r.message.account;
-		    
-                } else {
-                    frappe.msgprint( __("Please set the <b>default payable bank account</b> in the company.") );
-                }
-            }
-        }); 
-        frappe.call({
-            method: 'erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard.get_receivable_account',
-            callback: function(r) {
-                if ((r.message) && (r.message.account != "")) {
-                    document.getElementById("receivable_account").value = r.message.account;
-                } else {
-                    frappe.msgprint( __("Please set the <b>default receivable bank account</b> in the company.") );
-                }
-            }
-        }); 
-        frappe.call({
-            method: 'erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard.get_default_customer',
-            callback: function(r) {
-                if ((r.message) && (r.message.customer != "")) {
-                    document.getElementById("default_customer").value = r.message.customer;
-                } else {
-                    frappe.msgprint( __("Please set the <b>default customer</b> in <a href=\"/desk#Form/ERPNextSwiss Settings\">ERPNextSwiss Settings</a>.") );
-                }
-            }
-        }); 
-        frappe.call({
-            method: 'erpnextswiss.erpnextswiss.page.bank_wizard.bank_wizard.get_default_supplier',
-            callback: function(r) {
-                if ((r.message) && (r.message.supplier != "")) {
-                    document.getElementById("default_supplier").value = r.message.supplier;
-                } else {
-                    frappe.msgprint( __("Please set the <b>default supplier</b> in <a href=\"/desk#Form/ERPNextSwiss Settings\">ERPNextSwiss Settings</a>.") );
-                }
-            }
-        }); 
         */
     },
     make_chart: function() {
         const chart = new frappeChart.Chart( "#main-chart", { 
             data: {
-                labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
-                    "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+                labels: ["Jan", "Feb", "Mär", "Apr",
+                    "Mai", "Jun", "Jul", "Aug",
+                    "Sep", "Okt", "Nov", "Dez"],
 
                 datasets: [
                     {
-                        name: "Some Data", chartType: 'bar',
-                        values: [25, 40, 30, 35, 8, 52, 17, -4]
+                        name: __("Revenue YTD"), chartType: 'line',
+                        values: [5, 10, 15, 12, 23, 12, 7, 11, 12, 15, 9, 10]
                     },
-                    {
-                        name: "Another Set", chartType: 'bar',
-                        values: [25, 50, -10, 15, 18, 32, 27, 14]
+                                        {
+                        name: __("Revenue PY"), chartType: 'line',
+                        values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
                     },
-                    {
-                        name: "Yet Another", chartType: 'line',
+                                        {
+                        name: __("Expenses YTD"), chartType: 'line',
                         values: [15, 20, -3, -15, 58, 12, -17, 37]
+                    },
+                                        {
+                        name: __("Expenses PY"), chartType: 'line',
+                        values: [6, 7, 6, 8, 9, 10, 12, 8, 6, 7, 8, 5]
                     }
                 ],
 
-                yMarkers: [{ label: "Marker", value: 70,
-                    options: { labelPos: 'left' }}],
-                yRegions: [{ label: "Region", start: -10, end: 50,
-                    options: { labelPos: 'right' }}]
+                /*yMarkers: [{ label: "Marker", value: 70, options: { labelPos: 'left' }}], */
+                /*yRegions: [{ label: "Region", start: -10, end: 50, options: { labelPos: 'right' }}] */
             },
-
-            title: "My Awesome Chart",
-            type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+            type: 'line', 
             height: 300,
-            colors: ['purple', '#ffa3ef', 'light-blue'],
+            colors: ['#2ECC40', '#01FF70', '#85144B', '#FF851B'],
 
             tooltipOptions: {
                 formatTooltipX: d => (d + '').toUpperCase(),
-                formatTooltipY: d => d + ' pts',
+                formatTooltipY: d => d + ' EUR',
             }
         });
         
--- finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.py
+++ finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.py
@@ -5,4 +5,87 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from calendar import monthrange
 
+def get_cashflow_to_date(cost_center, py=False, income=True):
+    if py:
+        d = datetime.now() - relativedelta(years=1)
+    else:
+        d = datetime.now()
+    return get_cashflow("{y}-01-01".format(y=d.year), 
+        "{y}-{m}-{d}".format(y=d.year, m=d.month, d=d.day),
+        cost_center, income)
+                
+def get_cashflow_per_month(cost_center, py=False, income=True):
+    d = datetime.now()
+    x, feb = monthrange(d.year,2)
+    cashflows = [
+        get_cashflow("{y}-01-01".format(y=d.year), "{y}-01-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-02-01".format(y=d.year), "{y}-02-{f}".format(y=d.year, f=feb), cost_center, income),
+        get_cashflow("{y}-03-01".format(y=d.year), "{y}-03-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-04-01".format(y=d.year), "{y}-04-30".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-05-01".format(y=d.year), "{y}-05-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-06-01".format(y=d.year), "{y}-06-30".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-07-01".format(y=d.year), "{y}-07-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-08-01".format(y=d.year), "{y}-08-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-09-01".format(y=d.year), "{y}-09-30".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-10-01".format(y=d.year), "{y}-10-31".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-11-01".format(y=d.year), "{y}-11-30".format(y=d.year), cost_center, income),
+        get_cashflow("{y}-12-01".format(y=d.year), "{y}-12-31".format(y=d.year), cost_center, income)
+    ]
+    return cashflows
+                  
+def get_cashflow(from_date, to_date, cost_center, income=True):
+    if income:
+        t = "Income Account"
+    else:
+        t = "Expense Account"
+        # overhead costs    
+        sql_query_costs_overhead = """SELECT 
+                      IFNULL(SUM(`tabBudget Overhead`.`rate_per_month`), 0) AS `costs_overhead`
+                    FROM `tabBudget Overhead`
+                    WHERE 
+                      `tabBudget Overhead`.`docstatus` = 1
+                      /* AND `tabBudget Overhead`.`group` LIKE '%Sales%' */
+                      AND `tabBudget Overhead`.`cost_center` LIKE '{cost_center}'
+                      AND `tabBudget Overhead`.`start_date` <= '{from_date}'
+                      AND `tabBudget Overhead`.`end_date` >= '{to_date}';""".format(cost_center=cost_center, from_date=from_date, to_date=to_date)
+        try:
+            costs_overhead = frappe.db.sql(sql_query_costs_overhead, as_dict = True)[0]['costs_overhead']
+        except:
+            costs_overhead = 0.0
+    sql_query = """SELECT 
+              IFNULL(SUM(`tabGL Entry`.`credit`) - SUM(`tabGL Entry`.`debit`), 0) AS `cashflow`
+            FROM `tabGL Entry`
+            JOIN `tabAccount` ON `tabGL Entry`.`account` = `tabAccount`.`name`
+            WHERE 
+              `tabGL Entry`.`docstatus` = 1
+              AND `tabAccount`.`account_type` = '{t}'
+              AND `tabGL Entry`.`cost_center` LIKE '{cost_center}'
+              AND `tabGL Entry`.`posting_date` >= '{from_date}'
+              AND `tabGL Entry`.`posting_date` <= '{to_date}';""".format(t=t, cost_center=cost_center, from_date=from_date, to_date=to_date)
+    try:
+        cashflow = frappe.db.sql(sql_query, as_dict = True)[0]['cashflow']
+    except:
+        cashflow = 0.0
+    if income:
+        return cashflow
+    else:
+        return ((-1) * cashflow) + costs_overhead
+
+@frappe.whitelist()
+def get_cashflows(cost_center):
+    cashflows = {
+        'revenue_ytd': get_cashflow_to_date(cost_center, py=False, income=True),
+        'revenue_py': get_cashflow_to_date(cost_center, py=True, income=True),
+        'expenses_ytd': get_cashflow_to_date(cost_center, py=False, income=False),
+        'expenses_py': get_cashflow_to_date(cost_center, py=True, income=False),
+        'monthly_revenue_ytd': get_cashflow_per_month(cost_center, py=False, income=True),
+        'monthly_revenue_py': get_cashflow_per_month(cost_center, py=True, income=True),
+        'monthly_expenses_ytd': get_cashflow_per_month(cost_center, py=False, income=False),
+        'monthly_expenses_py': get_cashflow_per_month(cost_center, py=True, income=False)
+    }
+    print("{0}".format(cashflows))
+    return cashflows
