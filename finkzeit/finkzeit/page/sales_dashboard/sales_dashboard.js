0cc6c03e6fc74255500c3c6288a4111815ff18eb
lasalesi <larsericmueller@gmail.com>
2019-03-06 23:27:13 +0100

    sample chart

--- finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.js
+++ finkzeit/finkzeit/page/sales_dashboard/sales_dashboard.js
@@ -88,6 +88,8 @@ frappe.sales_dashboard = {
         */
     },
     run: function() {
+        frappe.sales_dashboard.make_chart();
+        //make_chart()
         /*
         // populate bank accounts
         frappe.call({
@@ -156,5 +158,45 @@ frappe.sales_dashboard = {
             }
         }); 
         */
+    },
+    make_chart: function() {
+        const chart = new frappeChart.Chart( "#main-chart", { 
+            data: {
+                labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
+                    "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+
+                datasets: [
+                    {
+                        name: "Some Data", chartType: 'bar',
+                        values: [25, 40, 30, 35, 8, 52, 17, -4]
+                    },
+                    {
+                        name: "Another Set", chartType: 'bar',
+                        values: [25, 50, -10, 15, 18, 32, 27, 14]
+                    },
+                    {
+                        name: "Yet Another", chartType: 'line',
+                        values: [15, 20, -3, -15, 58, 12, -17, 37]
+                    }
+                ],
+
+                yMarkers: [{ label: "Marker", value: 70,
+                    options: { labelPos: 'left' }}],
+                yRegions: [{ label: "Region", start: -10, end: 50,
+                    options: { labelPos: 'right' }}]
+            },
+
+            title: "My Awesome Chart",
+            type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+            height: 300,
+            colors: ['purple', '#ffa3ef', 'light-blue'],
+
+            tooltipOptions: {
+                formatTooltipX: d => (d + '').toUpperCase(),
+                formatTooltipY: d => d + ' pts',
+            }
+        });
+        
+        setTimeout(function() { chart.draw(!0)}, 1);
     }
 }
