beb18c2772e84d6423a68a716a8ed3594bbf2be5
lasalesi <larsericmueller@gmail.com>
2019-05-21 10:27:14 +0200

    added Wartungsvertrag doctype, v1.4.0

---
+++ finkzeit/finkzeit/doctype/wartungsvertrag/wartungsvertrag.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Fink Zeitsysteme/libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Wartungsvertrag', {
+	refresh: function(frm) {
+
+	}
+});
