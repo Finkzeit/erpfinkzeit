7114003092e0eb406c18e2e63d72bab02d717705
lasalesi <larsericmueller@gmail.com>
2020-07-06 18:16:48 +0200

    improved validation for tax accounts

--- finkzeit/finkzeit/doctype/kassa/kassa.js
+++ finkzeit/finkzeit/doctype/kassa/kassa.js
@@ -1,24 +1,37 @@
-// Copyright (c) 2018, Fink Zeitsysteme/libracore and contributors
+// Copyright (c) 2018-2020, Fink Zeitsysteme/libracore and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Kassa', {
 	refresh: function(frm) {
 
 	},
+    before_save: function(frm) {
+        frm.doc.cash_ins.forEach(function(entry) {
+            if ((entry.tax_amount > 0) && (!entry.tax_account)) {
+                frappe.validated = false;
+                frappe.msgprint("Bitte Steuerkonto angeben. Eingänge Zeile: " + entry.idx);
+            }
+        });
+        frm.doc.cash_outs.forEach(function(entry) {
+            if ((entry.tax_amount > 0) && (!entry.tax_account)) {
+                frappe.validated = false;
+                frappe.msgprint("Bitte Steuerkonto angeben. Ausgänge Zeile: " + entry.idx);
+            }
+        });          
+    }, 
     before_submit: function(frm) {
         frm.doc.cash_ins.forEach(function(entry) {
             if (!entry.in_account) {
                 frappe.validated = false;
-                frappe.msgprint("Bitte Eingangskonten angeben.");
+                frappe.msgprint("Bitte Eingangskonten angeben. Eingänge Zeile: " + entry.idx);
             }
         });
         frm.doc.cash_outs.forEach(function(entry) {
             if (!entry.out_account) {
                 frappe.validated = false;
-                frappe.msgprint("Bitte Ausgangskonten angeben.");
+                frappe.msgprint("Bitte Ausgangskonten angeben. Ausgänge Zeile: " + entry.idx);
             }
         });
-        
     },
     on_submit: function(frm) {
         if (frm.doc.docstatus == 1) {
