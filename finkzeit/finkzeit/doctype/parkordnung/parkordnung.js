b822ad260cc29c29b7b7c0649fb591e354bc5a56
lasalesi <larsericmueller@gmail.com>
2022-10-05 14:48:38 +0200

    Update parkordnung.js
    Bugfix: user name remains when clearing a user

--- finkzeit/finkzeit/doctype/parkordnung/parkordnung.js
+++ finkzeit/finkzeit/doctype/parkordnung/parkordnung.js
@@ -1,8 +1,13 @@
-// Copyright (c) 2019, Fink Zeitsysteme/libracore and contributors
+// Copyright (c) 2019-2022, Fink Zeitsysteme/libracore and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Parkordnung', {
 	refresh: function(frm) {
 
+	},
+	user: function(frm) {
+		if (!frm.doc.user) {
+			cur_frm.set_value('user_name', null);
+		}
 	}
 });
