acfbb5ab7585c693186baab6660f9e3347c56c0f
lasalesi <larsericmueller@gmail.com>
2023-08-02 21:17:24 +0200

    key management

--- finkzeit/finkzeit/doctype/transponder_configuration/transponder_configuration.js
+++ finkzeit/finkzeit/doctype/transponder_configuration/transponder_configuration.js
@@ -26,7 +26,60 @@ cur_frm.fields_dict['licence'].get_query = function(doc) {
     
 frappe.ui.form.on('Transponder Configuration', {
     refresh: function(frm) {
-
+        // set only once password fields: lock if set
+        if (frm.doc.project_pw) {
+            cur_frm.set_df_property('project_pw', 'read_only', 1);
+        }
+        if (frm.doc.wavenet_pw) {
+            cur_frm.set_df_property('wavenet_pw', 'read_only', 1);
+        }
+        if (frm.doc.lock_pw) {
+            cur_frm.set_df_property('lock_pw', 'read_only', 1);
+        }
+        if (frm.doc.key_a) {
+            cur_frm.set_df_property('key_a', 'read_only', 1);
+        }
+        if (frm.doc.key_b) {
+            cur_frm.set_df_property('key_b', 'read_only', 1);
+        }
+        if (frm.doc.master_key) {
+            cur_frm.set_df_property('master_key', 'read_only', 1);
+        }
+        if (frm.doc.app_master_key) {
+            cur_frm.set_df_property('app_master_key', 'read_only', 1);
+        }
+        if (frm.doc.app_read_key) {
+            cur_frm.set_df_property('app_read_key', 'read_only', 1);
+        }
+        // buttons
+        frm.add_custom_button(__("Keys erzeugen"), function() {
+            create_keys(frm);
+        });
+        frm.add_custom_button(__("Projekt-PW"), function() {
+            copy_key(frm, 'project_pw');
+        }, __("Keys"));
+        frm.add_custom_button(__("Wavenet-PW"), function() {
+            copy_key(frm, 'wavenet_pw');
+        }, __("Keys"));
+        frm.add_custom_button(__("Lock-PW"), function() {
+            copy_key(frm, 'lock_pw');
+        }, __("Keys"));
+        frm.add_custom_button(__("Key A"), function() {
+            copy_key(frm, 'key_a');
+        }, __("Keys"));
+        frm.add_custom_button(__("Key B"), function() {
+            copy_key(frm, 'key_b');
+        }, __("Keys"));
+        frm.add_custom_button(__("Master Key"), function() {
+            copy_key(frm, 'master_key');
+        }, __("Keys"));
+        frm.add_custom_button(__("App Master Key"), function() {
+            copy_key(frm, 'app_master_key');
+        }, __("Keys"));
+        frm.add_custom_button(__("App Read Key"), function() {
+            copy_key(frm, 'app_read_key');
+        }, __("Keys"));
+        
     },
     mfcl: function(frm) {
         if (frm.doc.mfcl === 1) {
@@ -82,3 +135,31 @@ frappe.ui.form.on('Transponder Configuration', {
         }
     }
 });
+
+function create_keys(frm) {
+    frappe.call({
+        'method': 'create_keys',
+        'doc': frm.doc,
+        'callback': function(response) {
+            cur_frm.refresh_fields();
+            cur_frm.dirty();
+        }
+    });
+}
+
+function copy_key(frm, key) {
+    frappe.call({
+        'method': 'copy_key',
+        'doc': frm.doc,
+        'args': {
+            'key': key
+        },
+        'callback': function(response) {
+            navigator.clipboard.writeText(response.message).then(function() {
+                frappe.show_alert("Key in der Zwischenablage");
+              }, function() {
+                 frappe.show_alert("Kein Zugriff auf Zwischenablage");
+            });
+        }
+    });
+}
--- finkzeit/finkzeit/doctype/transponder_configuration/transponder_configuration.py
+++ finkzeit/finkzeit/doctype/transponder_configuration/transponder_configuration.py
@@ -3,8 +3,39 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from random import choice
+from frappe.utils.password import get_decrypted_password
 
 class TransponderConfiguration(Document):
-	pass
+    # create each key if not already set
+    def create_keys(self):
+        if not self.project_pw:
+            self.project_pw = get_hex_token(12)
+        if not self.wavenet_pw:
+            self.wavenet_pw = get_hex_token(12)
+        if not self.lock_pw:
+            self.lock_pw = get_hex_token(12)
+        if not self.key_a:
+            self.key_a = get_hex_token(12)
+        if not self.key_b:
+            self.key_b = get_hex_token(12)
+        if not self.master_key:
+            self.master_key = get_hex_token(32)
+        if not self.app_master_key:
+            self.app_master_key = get_hex_token(32)
+        if not self.app_read_key:
+            self.app_read_key = get_hex_token(32)
+            
+        return
+        
+    # decrypt and copy key
+    def copy_key(self, key):
+        password = get_decrypted_password(self.doctype, self.name, key, False)
+        return password
+
+def get_hex_token(n):
+    hex_string = "0123456789abcdef"
+    token = "".join([choice(hex_string) for x in range(n)])
+    return token
