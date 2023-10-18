bbd9c318fa4ea0570379533db20e05abbe8ca3ea
lasalesi <larsericmueller@gmail.com>
2023-10-18 10:51:13 +0200

    new parking for either user or employee

--- finkzeit/finkzeit/doctype/parkordnung/parkordnung.py
+++ finkzeit/finkzeit/doctype/parkordnung/parkordnung.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019, Fink Zeitsysteme/libracore and contributors
+# Copyright (c) 2019-2023, Fink Zeitsysteme/libracore and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -7,4 +7,11 @@ import frappe
 from frappe.model.document import Document
 
 class Parkordnung(Document):
-	pass
+    def before_save(self):
+        if not self.user:
+            self.user_name = ""
+        if not self.employee:
+            self.employee_name = ""
+        self.display_name = self.user_name or self.employee_name or ""
+        
+        return
