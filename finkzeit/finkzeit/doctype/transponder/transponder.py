e6f462e634bddfdd228fb097d3856464eb1b39e1
Lars <lars.mueller@libracore.com>
2025-09-24 13:43:43 +0000

    Fallback for manual transponder creation for customer and licence

--- finkzeit/finkzeit/doctype/transponder/transponder.py
+++ finkzeit/finkzeit/doctype/transponder/transponder.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Transponder(Document):
-	pass
+	def before_save(self):
+		# if customer and/or licence are missing, try to load from transponder config
+		if not self.customer or not self.licence and self.transponder_configuration:
+			config = frappe.get_doc("Transponder Configuration", self.transponder_configuration)
+			if not self.customer:
+				self.customer = config.customer
+			if not self.licence:
+				self.licence = config.licence
+		return
